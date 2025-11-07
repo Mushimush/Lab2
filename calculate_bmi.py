098ba233c9cacb2df445f01bb3c661d98a301717
Mushimush <stanleytanyr@gmail.com>
2025-11-07 08:57:46 +0800

    update return values

--- calculate_bmi.py
+++ calculate_bmi.py
@@ -14,7 +14,10 @@ def calculate_bmi(height, weight):
     # Determine BMI classification using conditional operators
     if bmi < 18.5:
         print("Weight Classification: Under Weight")
+        return -1
     elif bmi <= 25.0:
         print("Weight Classification: Normal Weight")
+        return 0
     else:
         print("Weight Classification: Over Weight")
+        return 1

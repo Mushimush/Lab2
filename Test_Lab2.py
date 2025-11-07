70814b709c979da6669a9a6ea0327f755afaa877
Mushimush <stanleytanyr@gmail.com>
2025-11-07 16:15:56 +0800

    pytest added

--- Test_Lab2.py
+++ Test_Lab2.py
@@ -2,32 +2,41 @@ import Lab2
 
 def test_calc_min_max_temperature():
     print("Test_calc_min_max_temperature")
+    # Arrange
     result = []
     input_list = [5, 67, 32, 15, 89, 23, 41]
     expected_result = [5, 89]
 
+    # Act
     result = Lab2.calc_min_max_temperature(input_list)
 
+    # Assert
     assert result == expected_result
 
 
-def calc_average_temperature():
+def test_calc_average_temperature():
+    # Arrange
     result = 0
     input_list = [10, 20, 30, 40, 50]
     expected_result = 30.0
 
+    # Act
     result = Lab2.calc_average_temperature(input_list)
 
+    # Assert
     assert result == expected_result
 
 
 def test_calc_median_temperature():
+    # Arrange
     result = 0
     # Test with odd number of elements
     input_list = [5, 15, 25, 35, 45]
     expected_result = 25
 
+    # Act
     result = Lab2.calc_median_temperature(input_list)
 
+    # Assert
     assert result == expected_result
 
